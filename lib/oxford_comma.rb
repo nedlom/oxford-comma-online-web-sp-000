6ffd4a39643c74cde4b0fa6754983d7146484a43
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-12 15:06:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
+=begin
 def oxford_comma(array)
+  if array.length < 3
+    array.join(' and ')
+  else
+    last_word = array.pop
+    array << 'and '
+    phrase = array.join(', ') << last_word
+  end
+end
+=end
 
+def oxford_comma(array)
+  if array.length > 2
+    x = array.join(', ')
+    y = x.rindex(',')
+    x[y + 1] = ' and '
+    x
+  else
+    array.join(' and ')
+  end
 end
